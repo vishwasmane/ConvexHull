537716b507ba2d60513691ba46c2d03a791c95e1
Vishwas <vishwasmane4@gmail.com>
2018-10-06 23:20:03 +0530

    Revert "Completed convex hull algorithm for 2D points cloud."
    This reverts commit 223487a85a7225a8d22f28e4aadfec828f23ddaa.

--- ConvexAlgorithm/ConvexHulAlgorithm.cs
+++ ConvexAlgorithm/ConvexHulAlgorithm.cs
@@ -19,99 +19,27 @@ namespace ConvexHullAlgorithm
         {
             List<Vector> convectHulPoints= new List<Vector>();
 
-            //1. Find initial point
-            //TODO : Lambda expression
+            //Find initial point
             Vector startPoint;
             if (!Helper.GetPointWithMinX(this.pointsCloud, out startPoint))
                 return convectHulPoints;
+            //Lambda expression
+            //startPoint = this.pointsCloud.Min(p => p.X);
 
-            convectHulPoints.Add(startPoint);
-
-            //2. Find second point
-            Vector secondPoint = new Vector();
-            if (FindSecondPoint(startPoint, out secondPoint))
-                convectHulPoints.Add(secondPoint);
-
-            //3. Find further points
-            if (convectHulPoints.Count < 2)
-                return convectHulPoints;
-
-            Vector nextPoint = new Vector();
-
-            while (!nextPoint.Equals(startPoint))
-            {
-                Vector prevPoint = convectHulPoints.ElementAt(convectHulPoints.Count - 2);
-                Vector currentPoint = convectHulPoints.ElementAt(convectHulPoints.Count - 1);
-
-                List<Vector> remainingPoints = this.pointsCloud.ToList();
-                remainingPoints.Remove(prevPoint);
-                remainingPoints.Remove(currentPoint);
-
-                if (FindNextPoint(prevPoint, currentPoint, remainingPoints, out nextPoint))
-                {
-                    convectHulPoints.Add(nextPoint);
-                    //break;
-                }
-            }
-
-
-            return convectHulPoints;
-        }
-
-        private bool FindNextPoint(Vector prevPoint, Vector currentPoint, List<Vector> remainingPoints, out Vector nextPoint)
-        {
-            nextPoint = new Vector();
-            Vector prevCurrentVector = new Vector(currentPoint.X - prevPoint.X, currentPoint.Y - prevPoint.Y);
-            prevCurrentVector.Normalize();
-
-            double maxDotProduct = double.MinValue;
-            foreach (Vector point in remainingPoints)
-            {
-                Vector currentNextVector = new Vector(point.X - currentPoint.X, point.Y - currentPoint.Y);
-                currentNextVector.Normalize();
-
-                double dotProduct = prevCurrentVector* currentNextVector;
-                if (dotProduct == 0)
-                {
-                    nextPoint = point;
-                    return true;
-                }
-
-                if (maxDotProduct < dotProduct)
-                {
-                    maxDotProduct = dotProduct;
-                    nextPoint = point;
-                }
-            }
-
-            if (maxDotProduct == double.MaxValue)
-                return false;
-
-            return true;
-        }
-
-        private bool FindSecondPoint(Vector startPoint, out Vector secondPoint)
-        {
             List<Vector> remainingPoints = this.pointsCloud.ToList();
             remainingPoints.Remove(startPoint);
             bool isSecondPointFound = false;
-            secondPoint = new Vector();
-            while (!isSecondPointFound || remainingPoints.Count > 0)
+            Vector secondPoint = new Vector();
+            while (!isSecondPointFound)
             {
                 Vector minXPoint = new Vector();
                 if (Helper.GetPointWithMinX(remainingPoints, out minXPoint))
                 {
-                    if (Helper.AllPointsCrossProductIsInSameDirection(startPoint, minXPoint, remainingPoints))
-                    {
-                        isSecondPointFound = true;
-                        secondPoint = minXPoint;
-                        //convectHulPoints.Add(minXPoint);
-                        break;
-                    }
+                    Helper.AllPointsCrossProductIsInSameDirection(startPoint, minXPoint, remainingPoints);
                 }
             }
-
-            return isSecondPointFound;
+            return convectHulPoints;
         }
+        
     }
 }
